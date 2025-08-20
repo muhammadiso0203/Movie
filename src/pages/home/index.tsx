d401426e502d9a380c6f3e9f3450f0bc80bd91ce
muhammadiso0203 <muhammmadiso0203@gmail.com>
2025-08-20 08:21:16 +0500

    Movie

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,16 +1,18 @@
-import { memo } from 'react';
-import Hero from './components/hero';
-import Main from './components/main';
-import { useMovie } from '../movie/services/useMovie';
+import { memo } from "react";
+import Hero from "./components/hero";
+import { useMovie } from "../movie/services/useMovie";
+import MovieView from "../../shared/components/movie-view/MovieView";
 
 const Movie = () => {
-  const {getMovies} = useMovie()
-  const {data} = getMovies()
-  
+  const { getMovies } = useMovie();
+  const { data } = getMovies();
+
   return (
     <div>
-      <Hero/>
-      <Main data={data?.results}/>
+      <Hero />
+      <div className="mt-10">
+        <MovieView data={data?.results.slice(0, 4)} />
+      </div>
     </div>
   );
 };
